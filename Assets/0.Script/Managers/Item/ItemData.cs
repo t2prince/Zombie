140a6e03444032d2a1293432a75f0ac7c1438373
정재민 <t2prince@naver.com>
2025-05-31 10:20:59 +0900

    아이템 마스터 데이터 형태 추가

--- Assets/0.Script/Managers/Item/ItemData.cs
+++ Assets/0.Script/Managers/Item/ItemData.cs
@@ -6,6 +6,16 @@ namespace Jamcat.Managers.Item
     [CreateAssetMenu(fileName = "ItemData", menuName = "GameData/New ItemData", order = 0)]
     public class ItemData : BaseData
     {
+        public enum ItemType
+        {
+            None,
+            Gold,
+            Material,
+            Consumable,
+        }
         
+        public ItemType type;
+        public int count;
+        public GameObject itemPrefab;
     }
 }
