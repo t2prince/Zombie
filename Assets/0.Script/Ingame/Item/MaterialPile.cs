cc6a8d396a2c98eb24e2cd422266bacff6d74d32
정재민 <t2prince@naver.com>
2025-06-01 13:04:46 +0900

    좀비 몬스터 교체 및 콤퍼넌트 연결

--- Assets/0.Script/Ingame/Item/MaterialPile.cs
+++ Assets/0.Script/Ingame/Item/MaterialPile.cs
@@ -1,12 +1,16 @@
 using Jamcat.Ingame.Character;
+using UnityEngine;
 
 namespace Jamcat.Ingame.Item
 {
     public class MaterialPile : BaseCharacter
     {
+        [SerializeField] private float defence;
+        [SerializeField] private GameObject material;
         public override void TakeDamage(BaseCharacter attacher, float damage, float knockBackPower = 0.0f)
         {
-            hp -= damage;
+            var finalDamage = Mathf.Max(damage - defence, 0);
+            hp -= finalDamage;
             if (hp <= 0)
             {
                 Die();
