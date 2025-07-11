03ec1db5b2b452d7675c67fa0e35c4b92b728dd6
정재민 <t2prince@naver.com>
2025-07-12 00:04:40 +0900

    잡고 있던 물건을 던질 수 있게 수정

--- Assets/0.Script/Ingame/Player/PlayerGrabber.cs
+++ Assets/0.Script/Ingame/Player/PlayerGrabber.cs
@@ -1,4 +1,5 @@
 using Fusion.XR.Shared.Grabbing;
+using UnityEngine;
 
 namespace Ingame.Player
 {
@@ -7,15 +8,60 @@ namespace Ingame.Player
         public System.Action<Grabbable> onGrabbed;
         public System.Action<Grabbable> onUngrabbed;
 
+        private Vector3 _previousPosition;
+        private Vector3 _currentVelocity;
+        private Vector3 _previousRotation;
+        private Vector3 _currentAngularVelocity;
+
+        [SerializeField] private float _velocityMultiplier = 1.0f;
+        [SerializeField] private float _angularVelocityMultiplier = 1.0f;
+        [SerializeField] private float _maxDropVelocity = 10f;
+
+        private void Update()
+        {
+            if (grabbedObject != null)
+            {
+                Vector3 currentPosition = transform.position;
+                Vector3 currentRotation = transform.eulerAngles;
+                
+                _currentVelocity = (currentPosition - _previousPosition) / Time.deltaTime;
+                _currentAngularVelocity = (currentRotation - _previousRotation) / Time.deltaTime;
+                
+                _previousPosition = currentPosition;
+                _previousRotation = currentRotation;
+            }
+        }
+
         public override void Grab(Grabbable grabbable)
         {
             base.Grab(grabbable);
+            _previousPosition = transform.position;
+            _previousRotation = transform.eulerAngles;
+            _currentVelocity = Vector3.zero;
+            _currentAngularVelocity = Vector3.zero;
             onGrabbed?.Invoke(grabbable);
         }
 
         public override void Ungrab(Grabbable grabbable)
         {
             base.Ungrab(grabbable);
+            
+            var item = grabbable.GetComponent<Jamcat.Ingame.Item.Item>();
+            if (item != null)
+            {
+                var rigidbody = item.GetComponent<Rigidbody>();
+                if (rigidbody != null)
+                {
+                    Vector3 finalVelocity = _currentVelocity * _velocityMultiplier;
+                    Vector3 finalAngularVelocity = _currentAngularVelocity * _angularVelocityMultiplier;
+                    
+                    finalVelocity = Vector3.ClampMagnitude(finalVelocity, _maxDropVelocity);
+                    
+                    rigidbody.linearVelocity = finalVelocity;
+                    rigidbody.angularVelocity = finalAngularVelocity;
+                }
+            }
+            
             onUngrabbed?.Invoke(grabbable);
         }
     }
