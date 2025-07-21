3808835cc0f85f4ba2406a28df4a9c723e7dbbc3
정재민 <t2prince@naver.com>
2025-07-21 13:44:49 +0900

    미구현 인터페이스 함수 추가

--- Assets/0.Script/Ingame/InGame.cs
+++ Assets/0.Script/Ingame/InGame.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using Fusion;
+using Fusion.Sockets;
 using Fusion.XR.Shared.Rig;
 using Jamcat.Core;
 using Jamcat.Effect.ScreenEffect;
@@ -70,7 +72,7 @@ namespace Jamcat.Ingame
                 _players.Remove(playerRef);
             }
         }
-        
+
         private NetworkObject SpawnPlayer(PlayerRef playerRef)
         {
             var player = Loader.LoadPrefab<NetworkObject>(Loader.ResourceType.Avatars, "GamePlayer");
@@ -124,5 +126,39 @@ namespace Jamcat.Ingame
             _mapController = Util.SingletonUtil.GetSingletonComponent<MapController>();
             _monsterController = Util.SingletonUtil.GetSingletonComponent<MonsterController>();
         }
+        
+        #region INetworkRunnerCallbacks (debug log only)
+        public void OnConnectedToServer(NetworkRunner runner) {
+            Debug.Log("OnConnectedToServer");
+
+        }
+        public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+        {
+            Debug.Log("Shutdown: " + shutdownReason);
+        }
+        public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) {
+            Debug.Log("OnDisconnectedFromServer: "+ reason);
+        }
+        public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) {
+            Debug.Log("OnConnectFailed: " + reason);
+        }
+        #endregion
+
+        #region Unused INetworkRunnerCallbacks 
+
+        public void OnInput(NetworkRunner runner, NetworkInput input) { }
+        public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+        public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+        public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+        public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+        public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+        public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+        public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data) { }
+        public void OnSceneLoadDone(NetworkRunner runner) { }
+        public void OnSceneLoadStart(NetworkRunner runner) { }
+        public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+        public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+        public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) { }
+        public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
     }
 }
