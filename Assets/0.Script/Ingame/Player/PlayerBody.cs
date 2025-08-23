2ea869edd1e434012756033a66076a5378e825b1
정재민 <t2prince@naver.com>
2025-08-23 19:24:28 +0900

    스폰 로직 수정 중

--- Assets/0.Script/Ingame/Player/PlayerBody.cs
+++ Assets/0.Script/Ingame/Player/PlayerBody.cs
@@ -8,55 +8,67 @@ using Jamcat.Ingame.Equipment;
 using Jamcat.Managers.Weapon;
 using Jamcat.Managers.Player;
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEngine.InputSystem;
+#endif
 
 namespace Jamcat.Ingame.Player
 {
     public class PlayerBody : NetworkBehaviour
     {
+        #region Serialized Fields
         [SerializeField] private Transform _head;
         [SerializeField] private Transform _body;
-        private Pocket _pocket;
+        #endregion
         
+        #region Public Properties
         public Transform Head => _head;
         public Transform Body => _body;
+        #endregion
 
+        #region Networked Properties
         [Networked] public int GunId { get; set; } = -1;
         [Networked] public int MeleeId { get; set; } = -1;
         [Networked] public int BoosterId { get; set; } = -1;
+        #endregion
         
+        #region Private Fields
+        private Pocket _pocket;
         private bool _weaponsSpawned = false;
         private PlayerRef _playerRef;
-
-        private Gun currentGun;
-        private Melee currentMelee;
-        private Barrier currentBarrier;
-        private Booster currentBooster;
-        private BaseCharacter character;
+        private BaseCharacter _character;
         private NetworkRig _networkRig;
         private HardwareRig _hardwareRig;
+        
+        private Gun _currentGun;
+        private Melee _currentMelee;
+        private Barrier _currentBarrier;
+        private Booster _currentBooster;
+        #endregion
 
 #if UNITY_EDITOR
-        private InputAction arrowKeyAction;
+        #region Editor Only Fields
+        private InputAction _arrowKeyAction;
         [SerializeField] private float moveSpeed = 2f;
+        #endregion
         private void Start()
         {
             // 방향 키 입력 액션 초기화
-            arrowKeyAction = new InputAction(type: InputActionType.PassThrough);
-            arrowKeyAction.AddCompositeBinding("2DVector")
+            _arrowKeyAction = new InputAction(type: InputActionType.PassThrough);
+            _arrowKeyAction.AddCompositeBinding("2DVector")
                 .With("Up", "<Keyboard>/upArrow")
                 .With("Down", "<Keyboard>/downArrow")
                 .With("Left", "<Keyboard>/leftArrow")
                 .With("Right", "<Keyboard>/rightArrow");
-            arrowKeyAction.Enable();
+            _arrowKeyAction.Enable();
             
-            character = GetComponent<BaseCharacter>();
+            _character = GetComponent<BaseCharacter>();
         }
         
         public override void FixedUpdateNetwork()
         {
             // 네트워크 권한이 없으면 이동 로직을 실행하지 않음
-            if (character != null && !character.Object.HasInputAuthority) return;
+            if (_character != null && !_character.Object.HasInputAuthority) return;
             
             PerformMovement();
         }
@@ -70,179 +82,250 @@ namespace Jamcat.Ingame.Player
         private void PerformMovement()
         {
             // 방향 키 입력 값 읽기
-            var input = arrowKeyAction.ReadValue<Vector2>();
-            float deltaTime = Application.isEditor ? Time.deltaTime : (character?.Object?.Runner?.DeltaTime ?? Time.deltaTime);
+            var input = _arrowKeyAction.ReadValue<Vector2>();
+            float deltaTime = Application.isEditor ? Time.deltaTime : (_character?.Object?.Runner?.DeltaTime ?? Time.deltaTime);
             var move = Vector3.forward * input.y * moveSpeed * deltaTime;
 
             // 앞뒤 이동 (로컬 좌표계 기준)
             transform.Translate(move, Space.Self);
 
             // 좌우 회전
-            var rotation = input.x * moveSpeed * 100f * deltaTime;
+            var rotation = input.x * 100f * moveSpeed * deltaTime;
             transform.Rotate(Vector3.up, rotation);
         }
 #endif
         
 
+        #region Initialization
         public void Init(HardwareRig hardwareRig, NetworkRig networkRig, PlayerRef playerRef)
+        {
+            InitializeReferences(hardwareRig, networkRig, playerRef);
+            SetupPocket();
+            SetupGrabberEvents(hardwareRig);
+            InitializeCharacter();
+            RequestWeaponSpawnIfLocalPlayer(playerRef);
+        }
+
+        private void InitializeReferences(HardwareRig hardwareRig, NetworkRig networkRig, PlayerRef playerRef)
         {
             _networkRig = networkRig;
             _hardwareRig = hardwareRig;
             _playerRef = playerRef;
-            _pocket = networkRig.GetComponentInChildren<Pocket>();
+        }
+
+        private void SetupPocket()
+        {
+            _pocket = _networkRig.GetComponentInChildren<Pocket>();
             _pocket.gameObject.SetActive(false);
-            
-            if (hardwareRig != null)
+        }
+
+        private void SetupGrabberEvents(HardwareRig hardwareRig)
+        {
+            if (hardwareRig == null) return;
+
+            var grabbers = hardwareRig.GetComponentsInChildren<PlayerGrabber>();
+            foreach (var grabber in grabbers)
             {
-                var grabbers = hardwareRig.GetComponentsInChildren<PlayerGrabber>();
-                foreach (var grabber in grabbers)
-                {
-                    grabber.onGrabbed += (g) =>
-                    {
-                        var item = grabber.GetComponent<Item.Item>();
-                        if (item == null || item.type == Item.Item.ITemType.Use) return;
-                        
-                        _pocket.gameObject.SetActive(true);
-                    };    
-                    
-                    grabber.onUngrabbed += (g) =>
-                    {
-                        _pocket.gameObject.SetActive(false);
-                    };   
-                }
+                grabber.onGrabbed += _ => OnItemGrabbed(grabber);
+                grabber.onUngrabbed += _ => OnItemUngrabbed();
             }
+        }
+
+        private void OnItemGrabbed(PlayerGrabber grabber)
+        {
+            var item = grabber.GetComponent<Item.Item>();
+            if (item == null || item.type == Item.Item.ITemType.Use) return;
             
-            character = GetComponent<BaseCharacter>();
-            
-            // 로컬 플레이어인 경우에만 무기 정보 설정 후 호스트에 스폰 요청
+            _pocket.gameObject.SetActive(true);
+        }
+
+        private void OnItemUngrabbed()
+        {
+            _pocket.gameObject.SetActive(false);
+        }
+
+        private void InitializeCharacter()
+        {
+            _character = GetComponent<BaseCharacter>();
+        }
+
+        private void RequestWeaponSpawnIfLocalPlayer(PlayerRef playerRef)
+        {
             if (playerRef == InGame.Instance.Runner.LocalPlayer)
             {
                 var weapons = PlayerManager.GetWeapons();
-                Debug.Log($"[PlayerBody] Init - Requesting weapon spawn for local player - GunId:{weapons.gunId}, MeleeId:{weapons.meleeId}, BoosterId:{weapons.boosterId}");
+                Debug.Log($"[PlayerBody] Requesting weapon spawn - GunId:{weapons.gunId}, MeleeId:{weapons.meleeId}, BoosterId:{weapons.boosterId}");
                 RPC_RequestWeaponSpawn(weapons.gunId, weapons.meleeId, weapons.boosterId);
             }
             else
             {
-                Debug.Log($"[PlayerBody] Init - Remote player, waiting for weapon data sync");
+                Debug.Log("[PlayerBody] Remote player, waiting for weapon data sync");
             }
         }
+        #endregion
 
         public override void Spawned()
         {
             base.Spawned();
 
-            // InputAuthority가 있는 플레이어(로컬 플레이어)인 경우 카메라 설정
             if (Object.HasInputAuthority)
             {
-                var playerCamera = FindAnyObjectByType<PlayerFollowerCamera>();
-                if (playerCamera != null)
-                {
-                    playerCamera.Init(_head);
-                }
+                SetupPlayerCamera();
+            }
+        }
+
+        private void SetupPlayerCamera()
+        {
+            var playerCamera = FindAnyObjectByType<PlayerFollowerCamera>();
+            if (playerCamera != null)
+            {
+                playerCamera.Init(_head);
             }
         }
         
         [Rpc(RpcSources.StateAuthority, RpcTargets.InputAuthority)]
         public void RPC_SetNetworkRig(NetworkRig networkRig)
         {
-            // 이미 초기화되었으면 스킵
             if (_networkRig != null) return;
 
-            // InputAuthority가 있는 클라이언트에서만 실행
             if (Object.HasInputAuthority)
             {
-                var playerCamera = FindAnyObjectByType<PlayerFollowerCamera>();
-                HardwareRig hardwareRig = null;
-                
-                if (playerCamera != null)
-                {
-                    hardwareRig = playerCamera.GetComponentInChildren<HardwareRig>();
-                }
-                
-                // 초기화 실행
+                var hardwareRig = GetHardwareRig();
                 Init(hardwareRig, networkRig, Object.InputAuthority);
-                
-                // Locomotion도 같이 초기화
-                var locomotion = GetComponent<Locomotion.Locomotion>();
-                if (locomotion != null)
-                {
-                    locomotion.Init(networkRig, hardwareRig);
-                }
+                InitializeLocomotion(networkRig, hardwareRig);
             }
         }
+
+        private HardwareRig GetHardwareRig()
+        {
+            var playerCamera = FindAnyObjectByType<PlayerFollowerCamera>();
+            return playerCamera?.GetComponentInChildren<HardwareRig>();
+        }
+
+        private void InitializeLocomotion(NetworkRig networkRig, HardwareRig hardwareRig)
+        {
+            var locomotion = GetComponent<Locomotion.Locomotion>();
+            locomotion?.Init(networkRig, hardwareRig);
+        }
         
+        #region Network Callbacks
         public override void Render()
         {
             base.Render();
-            
-            // 원격 플레이어의 무기 정보가 동기화되면 스폰
-            if (!_weaponsSpawned && GunId >= 0 && MeleeId >= 0 && BoosterId >= 0)
+            CheckAndSpawnWeaponsForRemotePlayer();
+        }
+        
+        private void CheckAndSpawnWeaponsForRemotePlayer()
+        {
+            if (!_weaponsSpawned && HasValidWeaponIds())
             {
-                Debug.Log($"[PlayerBody] Render - Spawning weapons for remote player - GunId:{GunId}, MeleeId:{MeleeId}, BoosterId:{BoosterId}");
+                Debug.Log($"[PlayerBody] Spawning weapons for remote player - GunId:{GunId}, MeleeId:{MeleeId}, BoosterId:{BoosterId}");
                 SpawnWeapons();
                 _weaponsSpawned = true;
             }
         }
-        
+
+        private bool HasValidWeaponIds()
+        {
+            return GunId >= 0 && MeleeId >= 0 && BoosterId >= 0;
+        }
+        #endregion
+
+        #region Weapon Spawning
         [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
         public void RPC_RequestWeaponSpawn(int gunId, int meleeId, int boosterId)
         {
-            Debug.Log($"[PlayerBody] RPC_RequestWeaponSpawn - GunId:{gunId}, MeleeId:{meleeId}, BoosterId:{boosterId}");
+            Debug.Log($"[PlayerBody] Weapon spawn requested - GunId:{gunId}, MeleeId:{meleeId}, BoosterId:{boosterId}");
             
-            // 무기 정보 설정
+            SetWeaponIds(gunId, meleeId, boosterId);
+            SpawnWeapons();
+            _weaponsSpawned = true;
+        }
+
+        private void SetWeaponIds(int gunId, int meleeId, int boosterId)
+        {
             GunId = gunId;
             MeleeId = meleeId;
             BoosterId = boosterId;
-            
-            // 호스트에서 무기 스폰
-            SpawnWeapons();
-            _weaponsSpawned = true;
         }
         
         private void SpawnWeapons()
         {
-            if (_networkRig == null) return;
+            if (!CanSpawnWeapons()) return;
             
-            // 호스트 권한이 있을 때만 스폰
+            var (leftHand, rightHand, leftController, rightController) = GetHandComponents();
+            
+            SpawnGun(leftHand, leftController);
+            SpawnMelee(rightHand, rightController);
+            SpawnBooster(leftController, rightController);
+        }
+
+        private bool CanSpawnWeapons()
+        {
+            if (_networkRig == null) return false;
+            
             if (!Object.HasStateAuthority)
             {
-                Debug.LogWarning("[PlayerBody] SpawnWeapons - Only host can spawn weapons");
-                return;
+                Debug.LogWarning("[PlayerBody] Only host can spawn weapons");
+                return false;
             }
             
+            return true;
+        }
+
+        private (NetworkHand leftHand, NetworkHand rightHand, HandController leftController, HandController rightController) GetHandComponents()
+        {
             var leftHand = _networkRig.leftHand;
             var rightHand = _networkRig.rightHand;
             var leftController = leftHand.GetComponentInChildren<HandController>();
             var rightController = rightHand.GetComponentInChildren<HandController>();
             
-            //TODO: 플레이어 정보 보고 gun / melee / booster 연결해야함
-            //왼손 <-> 오른손 바꿀 수 있게끔 해줄 필요 있음
-            
-            // Gun 스폰
+            return (leftHand, rightHand, leftController, rightController);
+        }
+
+        private void SpawnGun(NetworkHand leftHand, HandController leftController)
+        {
             var gunData = WeaponManager.GetWeaponData(WeaponData.WeaponType.Gun, GunId);
             var gun = Runner.Spawn(gunData.weaponPrefab, leftHand.transform.position, leftHand.transform.rotation, _playerRef).GetComponent<Gun>();
-            gun.transform.SetParent(leftHand.transform);
-            gun.Init(character, leftController);
             
+            gun.transform.SetParent(leftHand.transform);
+            gun.Init(_character, leftController);
+            
+            SetGunFirePoint(gun, gunData);
+            _currentGun = gun;
+        }
+
+        private void SetGunFirePoint(Gun gun, WeaponData gunData)
+        {
             if (_hardwareRig != null)
             {
-                var position = _hardwareRig.leftHand.GetComponentInChildren<GunAttacher>().GetPosition(gunData.id);
-                gun.SetFirePoint(position);
+                var gunAttacher = _hardwareRig.leftHand.GetComponentInChildren<GunAttacher>();
+                var firePoint = gunAttacher.GetPosition(gunData.id);
+                gun.SetFirePoint(firePoint);
             }
-            
-            // Melee 스폰
+        }
+
+        private void SpawnMelee(NetworkHand rightHand, HandController rightController)
+        {
             var meleeData = WeaponManager.GetWeaponData(WeaponData.WeaponType.Melee, MeleeId);
             var meleeWeapon = Runner.Spawn(meleeData.weaponPrefab, rightHand.transform.position, rightHand.transform.rotation, _playerRef).GetComponent<Melee>();
-            meleeWeapon.transform.SetParent(rightHand.transform);
-            meleeWeapon.Init(character, rightController);
             
-            // Booster 스폰
+            meleeWeapon.transform.SetParent(rightHand.transform);
+            meleeWeapon.Init(_character, rightController);
+            _currentMelee = meleeWeapon;
+        }
+
+        private void SpawnBooster(HandController leftController, HandController rightController)
+        {
             var boosterData = WeaponManager.GetWeaponData(WeaponData.WeaponType.Booster, BoosterId);
             var boosterAttacher = _networkRig.GetComponentInChildren<Attacher>();
             var booster = Runner.Spawn(boosterData.weaponPrefab, boosterAttacher.transform.position, boosterAttacher.transform.rotation, _playerRef).GetComponent<Booster>();
+            
             booster.transform.SetParent(boosterAttacher.transform);
             booster.Init(this, leftController, rightController);
             booster.PlayerBody = _networkRig.GetComponentInChildren<NetworkHeadset>().transform;
+            _currentBooster = booster;
         }
+        #endregion
     }
 }
